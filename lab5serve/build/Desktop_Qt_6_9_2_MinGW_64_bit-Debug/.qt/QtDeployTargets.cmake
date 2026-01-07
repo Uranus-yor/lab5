ae84a312255b034bb387ea846120fae00881480a
Uranus-yor <zhanghaoran844@gmail.com>
2026-01-07 23:47:14 +0800

    sixth git

---
+++ lab5serve/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_lab5serve_FILE C:/Users/Uranus/Documents/lab5serve/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/lab5serve.exe)
+set(__QT_DEPLOY_TARGET_lab5serve_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_lab5serve_RUNTIME_DLLS C:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
